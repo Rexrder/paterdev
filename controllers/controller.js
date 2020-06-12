ac8b3919be06267dcb2bbf647bcb01c86bce72cd
Chris <lgnaitsirhc20@gmail.com>
2020-06-12 03:17:43 +0100

    commit

--- controllers/controller.js
+++ controllers/controller.js
@@ -6,8 +6,34 @@ controller.list = (req,res) =>{
          if (err) {
           res.json(err);
          }
-         res.render('index', {prod});
+         res.render('index', { prod });
         });
+    });
+};
+
+controller.delete = (req,res) =>{
+    const { id } = req.params;
+    req.getConnection((err, conn) => {
+        conn.query('DELETE FROM product WHERE id = ?', [id], (err, prod) => {
+         if (err) {
+          res.json(err);
+         }
+         res.redirect('/');
+        });
+    });
+};
+
+controller.redirect = (req,res) =>{
+    res.redirect('/');
+};
+
+controller.add = (req,res) =>{
+    const data = req.body;
+    console.log(req.body);
+    req.getConnection((err, conn) => {
+        conn.query('INSERT INTO product set ?', data, (err,prod) => {
+            res.redirect('/');
+        }); 
     });
 };
 
--- routes/routes.js
+++ routes/routes.js
@@ -1,6 +1,9 @@
 const router = require('express').Router();
-const controller = require('../controllers/controller')
+const controller = require('../controllers/controller');
 
-router.get('/',controller.list)
+router.get('/',controller.list);
+router.get('/delete/:id',controller.delete);
+router.post('/add',controller.add);
+router.get('/*',controller.redirect);
 
 module.exports = router;
