e2ef1a60091ba7afd6bedba519205382754dd0fb
Chris <lgnaitsirhc20@gmail.com>
2020-09-17 23:03:03 +0100

    commit

--- index.js
+++ index.js
@@ -14,7 +14,8 @@ app.set('views', path.join(__dirname, 'views'));
 app.engine('.hbs', exphbs({
   extname: 'hbs',
   defaultLayout: 'main',
-  layoutsDir: path.join(app.get('views'), 'layouts')
+  layoutsDir: path.join(app.get('views'), 'layouts'),
+  partialsDir: path.join(app.get('views'), 'partials')
 }))
 app.set('view engine', '.hbs');
 
